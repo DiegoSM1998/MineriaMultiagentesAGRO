6a09e5bc24f4182be17401b33636065614bbb17e
David Rivera Concepción <74268424+DavidRC17@users.noreply.github.com>
2022-12-20 19:47:34 +0100

    Add files via upload

--- src/docker_multiagentes/linear_regression/linear_regression.py
+++ src/docker_multiagentes/linear_regression/linear_regression.py
@@ -5,26 +5,25 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import psycopg2
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
 
 engine = create_engine("postgresql://docker:docker@host.docker.internal/AGROdb")
 
 query = '''SELECT * FROM "data_iterativo.txt.processed"'''
 df =pd.read_sql(query, engine)
-print(df)
-X = df[['Penetración (%)']]
-y = df['Valor (miles de €)']
 
-# Dividir el conjunto de datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(df)
+explicativas = df[['Consumo per capita']]
+objetivo = df['Gasto per capita']
 
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(X = explicativas, y = objetivo)
 
-predictions = model.predict(X_test)
-#df.insert(0,'prediction',predictions)
+predictions = model.predict(df[['Consumo per capita']])
+df.insert(0,'prediction',predictions)
 print(predictions)
-
-#sns.scatterplot(x='Penetración (%)', y='Valor (miles de €)', data = df)
-
-#sns.lineplot(x='Penetración (%)', y='prediction', data = df, color='red')
-#plt.show()
+print(df)
+sns.scatterplot(x='Consumo per capita', y='Gasto per capita', data = df)
+sns.scatterplot(x='Consumo per capita', y='prediction', data = df)
+plt.savefig('data/regression_model.png')
+plt.show()
