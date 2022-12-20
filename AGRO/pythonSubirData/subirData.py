4a25166bdea3cb26dee5a399d10d5e4145e7a9e2
Diego Sepúlveda Millán <diego.sepulveda@alu.uclm.es>
2022-12-20 20:15:40 +0100

    Delete AGRO directory

--- AGRO/pythonSubirData/subirData.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-import os
-import pandas as pd
-from sqlalchemy import create_engine
-
-#conecta con la base de datos existente en docker
-df = pd.read_csv("Data/Dataset1.V2_2013-20.txt", sep="|", decimal=",")
-engine= create_engine("postgresql://docker:docker@host.docker.internal/AGROdb")
-df.to_sql("Data1.Ampli.docker.raw",engine)
