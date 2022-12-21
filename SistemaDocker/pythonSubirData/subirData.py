9f8757930cc282a2150d8e16516f64b7a0dce38b
Diego Sepúlveda Millán <diego.sepulveda@alu.uclm.es>
2022-12-21 13:16:23 +0100

    Update subirData.py

--- SistemaDocker/pythonSubirData/subirData.py
+++ SistemaDocker/pythonSubirData/subirData.py
@@ -6,11 +6,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 #conecta con la base de datos existente en docker
-df = pd.read_csv("Data/Dataset1.V2_2013-20.txt", sep="|", decimal=",")
+df = pd.read_csv("Data/Dataset1.V2_2013-20.raw", sep="|", decimal=",")
 engine= create_engine("postgresql://docker:docker@host.docker.internal/AGROdb")
-df2 = pd.read_csv("Data/Dataset1.V1_2018-20.txt", sep="|", decimal=",")
-df3 = pd.read_csv("Data/dataModel.processed",sep=",")
+df2 = pd.read_csv("Data/Dataset1.V1_2018-20.raw", sep="|", decimal=",")
 df.to_sql("Data1.Ampli.raw",engine)
 df2.to_sql("Data1.raw",engine)
-df3.to_sql("dataModel.processed",engine)
-
